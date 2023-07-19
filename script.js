cf257d012a509ca4466b13cc266dadc8a6daac9b
shefroyrowe <shefroy.rowe@gmail.com>
2023-07-19 16:52:37 -0500

    simple u-i outlined

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const num1 = 0;
 const num2 = 0;
 const operator = '';
+const result = '';
+
+//arithmetic functions
+function add(num1, num2) {
+    return num1 + num2;
+ }
+
+ function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
 
 function operate (operator, num1, num2){
-    let result = '';
-    
     if(operator == '*'){
-        result = multiply(num1, num2);
+        result = multiply();
     }else if(operator == '+'){
-        result = sumNums(num1, num2);
+        result = add();
     }else if(operator == '-'){
-        result = subtract(num1, num2);
+        result = subtract();
     }else if(operator == '/'){
-        result = divide(num1, num2);
+        result = divide();
     }
 }
