c37d1a700ff1057abcc1d32c4065e2479f94cfbc
shefroyrowe <shefroy.rowe@gmail.com>
2023-07-20 14:23:53 -0500

    html structure update > complete U-I > added logic for updating display and for getting buttons clicked

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-const num1 = 0;
-const num2 = 0;
-const operator = '';
-const result = '';
+const calculator = {
+   displayValue: '0',
+   firstNum: null,
+   needSecondNum: false,
+   operator: null,
+};
 
-//arithmetic functions
-function add(num1, num2) {
-    return num1 + num2;
- }
-
- function subtract(num1, num2) {
-    return num1 - num2;
+function updateDisplay(){
+    //get display element
+    let display = document.querySelector('.display');
+    //update the value of the display >>
+    //>element with the contents of  'calculator displayVlue'
+    display.textContent = calculator.displayValue;
 }
 
-function multiply(num1, num2){
-    return num1 * num2;
-}
+updateDisplay();
 
-function divide(num1, num2){
-    return num1 / num2;
-}
+const calculatorButtons = document.querySelector('.calculator');
+calculatorButtons.addEventListener('click', (e)=>{
+    //get clicked element
+    const { target } = e;// is equivalent to (const target = event.target;)
 
-
-function operate (operator, num1, num2){
-    if(operator == '*'){
-        result = multiply();
-    }else if(operator == '+'){
-        result = add();
-    }else if(operator == '-'){
-        result = subtract();
-    }else if(operator == '/'){
-        result = divide();
+    //validate cliked element (if is button)
+    //if not button, exit function
+    if(!target.matches('button')){
+        return;
     }
+
+    if(target.classList.contains('operator')){
+        console.log('operator', target.value);
+        return;
+    }
+
+    if(target.classList.contains('decimal')){
+        console.log('decimal', target.value);
+        return;
+    }
+
+    if(target.classList.contains('all-clear')){
+        console.log('clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+    
+});
+
+function inputDigit(digit){
+    const { displayValue } = calculator; //same as calculator.displayValue
+    //change value of 'displayValue' if it's value is zero >>
+    //otherwise append to it
+
+    calculator.displayValue = displayValue === '0' ? digit :
+    displayValue + digit;
 }
