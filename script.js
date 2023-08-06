56e57b41015d99b87d3a4d462926d076930388f2
shefroyrowe <shefroy.rowe@gmail.com>
2023-08-05 23:57:14 -0500

    secondary display added > expression evaluting logig added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//math functions
-
+/*----------------*/
+  //math functions
+/*----------------*/
 function add (a,b){
     return a + b;
 }
@@ -14,4 +15,33 @@ function divide (a,b){
 
 function multi (a,b){
     return a * b;
+}
+
+/*---------------------*/
+  //operations variables
+/*---------------------*/
+const firstNumber = '';
+const secondNumber = '';
+const operator = '';
+
+/*--------------------*/
+  //calculate variable
+/*--------------------*/
+function operate (firstNumber, operator, secondNumber){
+    switch(operator){
+        case '+':
+            add(firstNumber, secondNumber);
+            break;
+        case '-':
+            sub(firstNumber, secondNumber);
+            break;
+        case '/':
+            divide(firstNumber, secondNumber);
+            break;
+        case '*':
+            multi(firstNumber, secondNumber);
+            break;
+        default:
+            console.log('ERROR');
+    }
 }
