a9a0e07f7d7eb6f9e6416f9a55b1339e95c43ede
shefroyrowe <shefroy.rowe@gmail.com>
2023-08-07 18:56:19 -0500

    ui redesign > updated ui color scheme script.js logic change

--- script.js
+++ script.js
@@ -1,71 +1,102 @@
 /*----------------*/
-  //math functions
+//math functions
 /*----------------*/
-function add (a,b){
+function add(a, b) {
     return a + b;
 }
 
-function sub (a,b){
+function sub(a, b) {
     return a - b;
 }
 
-function divide (a,b){
+function divide(a, b) {
     return a / b;
 }
 
-function multi (a,b){
+function multi(a, b) {
     return a * b;
 }
 
 /*---------------------*/
-  //operations object
+//operations object
 /*---------------------*/
-const updateDispaly = {
+const updateDisplay = {
     firstNumber: '',
     secondNumber: '',
     operator: '',
 };
 
 /*--------------------*/
-  //calculate variable
+//calculate variable
 /*--------------------*/
-function operate (firstNumber, operator, secondNumber){
-    switch(operator){
-        case '+':
-            add(firstNumber, secondNumber);
-            break;
-        case '-':
-            sub(firstNumber, secondNumber);
-            break;
-        case '/':
-            divide(firstNumber, secondNumber);
-            break;
-        case '*':
-            multi(firstNumber, secondNumber);
-            break;
-        default:
-            console.log('ERROR');
+function operate(a, operator, b) {
+    if(operator === '+'){
+        return a + b;
+    }
+
+    if(operator === '-'){
+        return a - b;
+    }
+
+    if(operator === '*'){
+        return a * b;
+    }
+
+    if(operator === '/'){
+        return a / b;
     }
 }
 
 /*----------------------------------------------*/
-  //show equation expression in secondary display
+//show equation expression in secondary display
 /*----------------------------------------------*/
-function evaluateEquation(){
+function evaluateEquation() {
     const keys = document.getElementsByTagName('button');
-    const secondaryDisplay = document.querySelector('.secondary-display');
+    const display = document.querySelector('.secondary-display');
+    const mainDisplay = document.querySelector('.display');
 
-     Array.from(keys).forEach(key => {
-        key.addEventListener('click', (e)=>{
-            let value = e.target.value;
+    Array.from(keys).forEach(key => {
+        key.addEventListener('click', (e) => {
+            let { value } = e.target;
 
-            if(value !== 'del' && value !== '='){
-                updateDispaly.firstNumber = updateDispaly.firstNumber += value;
-                secondaryDisplay.textContent = updateDispaly.firstNumber;
+            //get and display first operand value
+            if (key.classList.contains('number') &&
+                updateDisplay.operator === '') {
+
+                updateDisplay.firstNumber += value;
+                display.textContent = updateDisplay.firstNumber;
             }
-            
+
+            //get and append operator to firstnumber
+            if (isNaN(updateDisplay.firstNumber) === false &&
+                key.classList.contains('operator') &&
+                updateDisplay.operator === '') {
+
+                updateDisplay.operator = value;
+                display.textContent += updateDisplay.operator;
+            }
+
+            //get and append second operand value
+            if (isNaN(updateDisplay.firstNumber) === false &&
+                key.classList.contains('operator') === false &&
+                updateDisplay.operator !== '') {
+
+                updateDisplay.secondNumber = value;
+                display.textContent += updateDisplay.secondNumber;
+            }
+
+            //run operate function on input values
+            if (value === '=' &&
+                isNaN(updateDisplay.firstNumber) === false &&
+                isNaN(updateDisplay.secondNumber) === false &&
+                updateDisplay.operator !== '') {
+                display.textContent = operate(updateDisplay.firstNumber,
+                    updateDisplay.operator, updateDisplay.secondNumber);
+
+            }
+
         });
     });
 }
-
 evaluateEquation();
+
